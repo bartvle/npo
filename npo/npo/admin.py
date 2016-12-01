60b9c1dbb8f7eca87c904c9c3f508e8c914f949e
Bart Vleminckx <bartvle@users.noreply.github.com>
2016-12-01 16:30:01 +0100

    Change name of admin and add admin 'kaart' page

--- npo/npo/admin.py
+++ npo/npo/admin.py
@@ -5,12 +5,26 @@
 from django.contrib.admin import AdminSite
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.conf.urls import patterns, url
+from django.shortcuts import render
+
+
+def kaart(request):
+    """
+    """
+    return render(request, 'admin/kaart.htm')
 
 
 class AdminSite(AdminSite):
     """
     """
-    pass
+    site_header = 'Natuurpunt Oosterzele Admin'
+
+    def get_urls(self):
+        """
+        """
+        urls = [url(r'^kaart/$', kaart)]
+        return super(AdminSite, self).get_urls() + urls
 
 
 admin_site = AdminSite()
