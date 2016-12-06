54badafc74e890fd90474ca25a0e51795d6d6f32
Bart Vleminckx <bart.vleminckx@observatory.be>
2016-12-06 16:33:22 +0100

    Add list of newsletter subscriptions to admin

--- npo/npo/admin.py
+++ npo/npo/admin.py
@@ -7,12 +7,24 @@ from django.contrib.auth.models import User, Group
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from django.conf.urls import patterns, url
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+from newsletter.models import Subscription
 
 
 def kaart(request):
     """
     """
     return render(request, 'admin/kaart.htm')
+
+
+def make_list(request):
+    """
+    """
+    emails = [s.email for s in Subscription.objects.all()]
+    s = '; '.join(emails)
+    return HttpResponse(s)
 
 
 class AdminSite(AdminSite):
@@ -23,7 +35,7 @@ class AdminSite(AdminSite):
     def get_urls(self):
         """
         """
-        urls = [url(r'^kaart/$', kaart)]
+        urls = [url(r'^kaart/$', kaart), url(r'^inschrijvingen/$', make_list)]
         return super(AdminSite, self).get_urls() + urls
 
 
