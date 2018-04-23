7d90f1672c3cd0b327e64ae27532021e843f6d3c
Bart Vleminckx <bart.vleminckx@observatory.be>
2018-04-23 10:14:40 +0200

    Add newsletter subscription date

--- npo/newsletter/admin.py
+++ npo/newsletter/admin.py
@@ -10,6 +10,8 @@ from .models import Subscription
 
 
 class SubscriptionAdmin(ModelAdmin):
+    list_display  = ('email', 'created_at')
     list_per_page = 200
+    ordering = ('-created_at',)
 
 admin_site.register(Subscription, SubscriptionAdmin)
--- npo/newsletter/models.py
+++ npo/newsletter/models.py
@@ -11,6 +11,7 @@ class Subscription(models.Model):
     """
 
     email = models.EmailField(verbose_name=_('email'), unique=True)
+    created_at = models.DateTimeField(verbose_name=_('created at'), auto_now_add=True)
 
     class Meta:
         verbose_name = _('subscription')
