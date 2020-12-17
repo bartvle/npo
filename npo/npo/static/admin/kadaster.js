254cd9e4e3c10303e5dd1e9641a7da648bde455f
Bart Vleminckx <bart.vleminckx@outlook.com>
2020-12-17 21:41:53 +0100

    Get npo parcels dynamically

--- npo/npo/static/admin/kadaster.js
+++ npo/npo/static/admin/kadaster.js
@@ -93,7 +93,8 @@ function showData(e) {
 }
 
 function onEachFeature(feature, layer) {
-    if (feature.properties.Eigenaar == 'NPO') {
+    // if (feature.properties.Eigenaar == 'NPO') {
+    if (npo_parcels.includes(feature.properties.CAPAKEY)) {
         layer.setStyle(style_npo);
     } else if (feature.properties.Eigenaar == 'ANB') {
         layer.setStyle(style_anb);
