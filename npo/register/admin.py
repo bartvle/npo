8a60ad84dbb8ca8581d2aec5c0a52c9288d82e26
Bart Vleminckx <bart.vleminckx@outlook.com>
2022-06-05 15:16:22 +0200

    Update deprecated import

--- npo/register/admin.py
+++ npo/register/admin.py
@@ -2,7 +2,7 @@
 """
 
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.contrib.admin import ModelAdmin, TabularInline
 
 from npo.admin import admin_site
